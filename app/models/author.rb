d14c7d7abe804815cc2b20efe2c85be0c423a8e6
Yazeed <dee3ayahmed25@gmail.com>
2023-07-24 22:56:57 +0300

    completed assignment

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ApplicationRecord
+  has_many :posts
+
+  has_one :profile
+end
+
+class Profile < ApplicationRecord
+
+  belongs_to :Author
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  belongs_to :author
+  has_many :Post_tags
+  has_many :tags, through: :post_tags
+end
+
+class PostTag < ApplicationRecord
+  belongs_to :post
+  belongs_to :tag
+end
+
+class Tag < ApplicationRecord
+  has_many :post_tags
+   has_many :posts, through: :post_tags
 end
